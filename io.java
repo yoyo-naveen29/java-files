280b92d09b07aa1b7387c9ba91936b97d26f570a
yoyo-naveen29 <naveenkunchala76@gmail.com>
2023-09-05 14:43:35 +0530

    yoyo

---
+++ io.java
@@ -0,0 +1,14 @@
+class io
+{
+	public static void main(String[] args)
+	{
+		int n=15,start=1,a=0;
+		while(n>=start)
+		{
+			a=a+start;
+			System.out.println(start);
+			start++;
+		}
+			System.out.println("sum of natural numbers =  "  +  a);
+	}
+}
